fd753b914978f176746ceab61cbeebcfa711f0ba
blackweeed <blackweeeed@gmail.com>
2023-04-22 14:30:11 +0200

    headnling an error

--- client.js
+++ client.js
@@ -1,6 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
+  // uri: process.env.WP_GRAPHQL_URL,
   uri: process.env.WP_GRAPHQL_URL,
   cache: new InMemoryCache(),
 });
