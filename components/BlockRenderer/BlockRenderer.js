8cefb8e848fe10ecc88ee51a657a35d148fbb315
blackweeed <blackweeeed@gmail.com>
2023-04-24 07:16:03 +0200

    fetching pages data from wordpress

--- components/BlockRenderer/BlockRenderer.js
+++ components/BlockRenderer/BlockRenderer.js
@@ -1,12 +1,24 @@
 import { Cover } from "components/Cover";
+import { Heading } from "components/Heading";
 
 export const BlockRender = ({ blocks }) => {
   return blocks.map((block) => {
     switch (block.name) {
+      case "core/heading": {
+        return (
+          <Heading
+            key={block.id}
+            level={block.attributes.level}
+            content={block.attributes.content}
+            textAlign={block.attributes.textAlign}
+          />
+        );
+      }
       case "core/cover": {
+        console.log(block);
         return (
           <Cover key={block.id} background={block.attributes.url}>
-            core cover
+            <BlockRender blocks={block.innerBlocks} />
           </Cover>
         );
       }
